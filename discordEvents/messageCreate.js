2cf0b151ae93036ae422feb66ecae4a7eb02d848
Sidhant Shahi <starindustries05@gmail.com>
2022-10-09 22:00:11 +0530

    added temp functionality for editing and repeating messages

--- discordEvents/messageCreate.js
+++ discordEvents/messageCreate.js
@@ -1,7 +1,16 @@
 const client = require("..");
 
 client.on('messageCreate', async (message) => {
-    // console.log(message.content);
+    if(message.content.startsWith('!edit')) {
+        args = message.content.split(' ');
+        const msg = await message.channel.messages.fetch(args[1]);
+        await msg.edit(args.slice(2).join(' '));
+        message.delete();
+    }
+    else if(message.content.startsWith("!")){
+        await message.channel.send(message.content.slice(1));
+        message.delete();
+    }
 });
 
 module.exports = {name:"messageCreate"}
